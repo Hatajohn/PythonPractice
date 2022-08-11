d8296fc1cc7643edd08f04ee349197231a03057f
hatajm <hatajm@miamioh.edu>
2022-08-11 15:52:49 -0400

    I don't feel like messing with 3 dimensions right off the batt

--- Playground.py
+++ Playground.py
@@ -12,26 +12,27 @@ class Ant:
         self.caste = caste
         self.position_x = 0
         self.position_y = 0
-        self.position_z = 0
+        #self.position_z = 0
 
     def move_rand(self):
         self.position_x = rand.randrange(-10,10,1)
         self.position_y = rand.randrange(-10,10,1)
-        self.position_z = rand.randrange(-10,10,1)
+        #self.position_z = rand.randrange(-10,10,1)
 
-    def move(self, x, y, z):
+    def move(self, x, y):
         self.position_x += x
         self.position_y += y
-        self.position_z += z
+        #self.position_z += z
 
     def position(self):
-        return "{}'s position is: ({}, {}, {})".format(self.name, self.position_x, self.position_y, self.position_z)
+        return "{}'s position is: ({}, {})".format(self.name, self.position_x, self.position_y)
+        #return "{}'s position is: ({}, {}, {})".format(self.name, self.position_x, self.position_y, self.position_z)
 
 class Colony:
     newid = itertools.count().__next__
     def __init__(self, space) -> None:
         self.id = Colony.newid()
-        self.locations = np.array((space, space, space))
+        self.locations = np.array((space, space))
         self.ants = []
         print("Colony {} established!".format(self.id))
     
@@ -40,21 +41,30 @@ class Colony:
         print("{} was added to Colony {}".format(ant.name, self.id))
     
     def size(self):
-        print("Colony {} has {} ants".format(self.id, self.ants.count))
+        print("Colony {} has {} ants".format(self.id, len(self.ants)))
+
+class Simulation:
+    id = itertools.count().__next__
+    def __init__(self, iters):
+        self.id = Simulation.id()
+        self.iters = iters
 
 # Driver
+def main():
+    timmy = Ant("Timothy", "worker")
+    jimmy = Ant("Jimothy", "soldier")
 
-timmy = Ant("Timothy", "worker")
-jimmy = Ant("Jimothy", "soldier")
+    # string format
+    print("Say hello to {}!".format(timmy.name))
+    print("{} is a {} ant.".format(timmy.name, timmy.caste))
 
-# string format
-print("Say hello to {}!".format(timmy.name))
-print("{} is a {} ant.".format(timmy.name, timmy.caste))
+    print("Say hello to {}!".format(jimmy.name))
+    print("{} is a {} ant.".format(jimmy.name, jimmy.caste))
 
-print("Say hello to {}!".format(jimmy.name))
-print("{} is a {} ant.".format(jimmy.name, jimmy.caste))
+    cleveland = Colony(1000)
+    cleveland.ant_add(timmy)
+    cleveland.ant_add(jimmy)
+    cleveland.size()
 
-cleveland = Colony(1000)
-cleveland.ant_add(timmy)
-cleveland.ant_add(jimmy)
-cleveland.size()
+if __name__ == "__main__":
+    main()
